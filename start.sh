8366802d94326cd480d30c4a4ffd77a327419300
mada659 <35201719+mada659@users.noreply.github.com>
2026-03-31 21:25:49 -0400

    start.sh chg

--- start.sh
+++ start.sh
@@ -2,12 +2,15 @@
 
 # Clone additional repos for experimenting with SysMLv2
 if [ ! -d "./sysmlv2" ]; then
+  # SysML jupyter env is already installed with the jupyter docker container. This is just to have access to the documentation and sample files.
   git clone https://github.com/Systems-Modeling/SysML-v2-Release.git ./sysmlv2
 fi
 if [ ! -d "./example_sysml_notebooks" ]; then
+# Some good examples of Jupyter notebooks for SysMLv2. This is not an official repo, but it has some good examples of using the SysMLv2 kernel in Jupyter.
   git clone https://github.com/MBSE4U/SysMLv2JupyterBook.git ./example_sysml_notebooks
 fi
 if [ ! -d "./example_sysml_files" ]; then
+# Some good examples of SysMLv2 files. This is not an official repo, but it has some good examples of a sequential jupyter build.
   git clone https://github.com/sensmetry/advent-of-sysml-v2.git ./example_sysml_files
 fi
 
